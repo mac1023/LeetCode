e0f5369a7ed164d1aa6399289312313fbe7b5fa6
张亚杰 <zhangyajie6@tal.com>
2021-11-05 17:14:58 +0800

    链表反转

--- 206-链表反转/1p/main.go
+++ 206-链表反转/1p/main.go
@@ -1,5 +1,26 @@
 package main
 
+//链表的结构
+type ListNode struct {
+	Value int
+	Next  *ListNode
+}
+
 func main() {
 
+}
+
+func reverselist(head *ListNode) *ListNode {
+	var prev *ListNode
+	curr := head
+
+	for curr != nil {
+		next := curr.Next
+		curr.Next = prev
+
+		prev = curr
+		curr = next
+	}
+
+	return prev
 }
